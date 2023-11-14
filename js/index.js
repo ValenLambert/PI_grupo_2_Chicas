4841b42009deb29778d9d50b3c949c5a616f3f1f
Valen Lambert <vlambert@udesa.edu.ar>
2023-11-14 20:20:53 -0300

    avanve en detalle_peliculas y search_results con qs

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ let serValor = document.querySelector ("#cuarta")
      for (let i = 0; i < 4; i++) { 
          let dato = data.results[i].title;
          peliculas += `<div class="peliculasvaloradas">
-             <a href="../PI_grupo_2_Chicas/detallePelicula.html">
+             <a href="../PI_grupo_2_Chicas/detallePelicula.html?idPersonaje=${data.results[i].id}">
              <img class="imagen" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" class="imagen"></img>
              <h3 class="titulospelicula" >${dato}</h3>
              <p class="tituloestreno">Fecha de estreno: ${data.results[i].release_date}</p>
@@ -51,7 +51,7 @@ let serValor = document.querySelector ("#cuarta")
      for (let i = 0; i < 4; i++) { 
          let dato2 = data.results[i].title;
          peliculas2 += `<div class="peliculasvaloradas">
-             <a href="../PI_grupo_2_Chicas/detallePelicula.html">
+             <a href="../PI_grupo_2_Chicas/detallePelicula.html?idVisto=${data.results[i].id}">
              <img class="imagen" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" class="imagen"></img>
              <h3 class="titulospelicula" >${dato2}</h3>
              <p class="tituloestreno">Fecha de estreno: ${data.results[i].release_date}</p>
--- js/search_results.js
+++ js/search_results.js
@@ -1,6 +1,35 @@
-let busqueda = document.querySelector (".form")
-let noResultados = document.querySelector (".Noresultados")
+let qs = location.search;
+let qsObj = new URLSearchParams (qs);
+let nombrePelicula = qsObj.get ("nombrePelicula");
+let titulo= document.querySelector (".Titulosbusqueda");
 
-busqueda.addEventListener ("keydowm", function(e){  
-    console.log (this.value)
+let pelis_valoradas = `https://api.themoviedb.org/3/movie/top_rated?api_key=42737f60c529bfe7e9586db8cb132a1c`; /*ARREGLAR PARA QUE SOLO TOME ESE*/
+let peliculasValor = document.querySelector (".padreValoradas");
+
+titulo.innerHTML= `Busqueda: <i class="fa-solid fa-magnifying-glass"> ${nombrePelicula}</i>`
+
+fetch(pelis_valoradas)
+.then(function (response) {
+    return response.json();
 })
+.then(function (data) {
+    console.log(data); 
+    let peliculas = "";
+    for (let i = 0; i < (data.results).length; i++) { 
+        let dato = data.results[i].title;
+        peliculas += `<div class="peliculasvaloradas">
+            <a href="../PI_grupo_2_Chicas/detallePelicula.html?idPersonaje=${data.results[i].id}">
+            <img class="imagen" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" class="imagen"></img>
+            <h3 class="titulospelicula" >${dato}</h3>
+            <p class="tituloestreno">Fecha de estreno: ${data.results[i].release_date}</p>
+            <a/>
+            </div>`;
+    }
+   
+    peliculasValor.innerHTML = peliculas;
+
+})
+.catch(function (error) {
+    console.log(`El error es ${error}`); 
+    return error;
+});
